993d104f4d24243f7f1704cac294d31abdeedaaf
HalKiyo <hasegawa.k.ai@m.titech.ac.jp>
2023-01-16 18:19:20 +0900

    draw_disc_multigrid is deployed

--- class/thailand/cnn3_main.py
+++ class/thailand/cnn3_main.py
@@ -1,19 +1,18 @@
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import pickle
 import numpy as np
 import tensorflow as tf
-from os.path import exists
 from tensorflow.python.framework.ops import disable_eager_execution
+disable_eager_execution()
 
 from model3 import build_model
 from view import draw_val, show_class, view_accuracy
 from util import load, shuffle, mask
 from gradcam import grad_cam, show_heatmap, image_preprocess
 
-disable_eager_execution()
-
 def main():
-    train_flag = True # modifiable
+    train_flag = False # modifiable
 
     px = Pixcel()
     if train_flag is True:
@@ -26,11 +25,11 @@ def main():
 
     px.validation()
     px.show(val_index=0)
-    px.label_dist(px_index=6)
+    px.label_dist_multigrid()
 
 class Pixcel():
     def __init__(self):
-        self.class_num = 30
+        self.class_num = 5
         self.descrete_mode = 'EFD'
         self.tors = 'predictors_coarse_std_Apr_msot'
         self.tant = f"pr_5x5_coarse_std_MJJASO_thailand_{self.descrete_mode}_{self.class_num}"
@@ -41,7 +40,7 @@ class Pixcel():
         self.lat_grid, self.lon_grid = 4, 4
         self.grid_num = self.lat_grid*self.lon_grid 
         self.batch_size = 256
-        self.epochs = 250
+        self.epochs = 150
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
         self.loss = tf.keras.losses.CategoricalCrossentropy()
         self.metrics = tf.keras.metrics.CategoricalAccuracy()
@@ -109,10 +108,10 @@ class Pixcel():
         y_val_px = y_val[val_index].reshape(4, 4)
         show_class(y_val_px, class_num=self.class_num)
 
-        model = build_model((self.lat, self.lon, self.var_num), self.class_num)
-        model.compile(optimizer=self.optimizer, loss=self.loss, metrics=[self.metrics])
         pred_lst = []
         for i in range(self.grid_num):
+            model = build_model((self.lat, self.lon, self.var_num), self.class_num)
+            model.compile(optimizer=self.optimizer, loss=self.loss, metrics=[self.metrics])
             weights_path = f"{self.weights_dir}/class{self.class_num}_epoch{self.epochs}_batch{self.batch_size}_{i}.h5"
             model.load_weights(weights_path)
             pred = model.predict(x_val)
@@ -134,6 +133,26 @@ class Pixcel():
         model.load_weights(weights_path)
         pred = model.predict(x_val)
         draw_val(pred, y_val_one_hot, class_num=self.class_num)
+
+    def label_dist_multigrid(self):
+        with open(self.savefile, 'rb') as f:
+            data = pickle.load(f)
+        x_val, y_val = data['x_val'], data['y_val']
+        pred_lst = []
+        y_val_lst = []
+        for i in range(self.grid_num):
+            y_val_px = y_val[:, i]
+            y_val_one_hot = tf.keras.utils.to_categorical(y_val_px, self.class_num)
+            model = build_model((self.lat, self.lon, self.var_num), self.class_num)
+            model.compile(optimizer=self.optimizer, loss=self.loss, metrics=[self.metrics])
+            weights_path = f"{self.weights_dir}/class{self.class_num}_epoch{self.epochs}_batch{self.batch_size}_{i}.h5"
+            model.load_weights(weights_path)
+            pred = model.predict(x_val)
+            pred_lst.append(pred)
+            y_val_lst.append(y_val_one_hot)
+        pred_arr = np.array(pred_lst).reshape(16*1000, 30)
+        y_val_arr = np.array(y_val_lst).reshape(16*1000, 30)
+        draw_val(pred_arr, y_val_arr, class_num=self.class_num)
 
 
 if __name__ == '__main__':
