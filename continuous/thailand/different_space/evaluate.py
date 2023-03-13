9e4496f1cca02ad32c4905694ba85178715fbc35
HalKiyo <hasegawa.k.ai@m.titech.ac.jp>
2023-03-13 17:15:32 +0900

    land ocean mask changes

---
+++ continuous/thailand/different_space/evaluate.py
@@ -0,0 +1,85 @@
+import os 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import warnings
+warnings.filterwarnings('ignore')
+import numpy as np
+
+from util import open_pickle
+from model import init_model
+from view import diff_bar, draw_val
+
+def main():
+    EVAL = evaluate()
+    x_val, y_val, pred = EVAL.load_pred()
+    if EVAL.diff_bar_view_flag is True:
+        EVAL.diff_evaluation(pred, y_val)
+    if EVAL.true_false_view_flag is True:
+        EVAL.true_false_bar(pred, y_val)
+
+class evaluate():
+    def __init__(self):
+        self.val_index = 20
+        self.epochs =100
+        self.batch_size =256
+        self.seed =1
+        self.var_num = 4
+        self.vsample = 1000
+        self.resolution = '1x1'
+        # path
+        self.tors = 'predictors_coarse_std_Apr_t'
+        self.tant = f"pr_{self.resolution}_std_MJJASO_thailand"
+        self.workdir = '/docker/mnt/d/research/D2/cnn3'
+        self.val_path = self.workdir + f"/train_val/continuous/{self.tors}-{self.tant}.pickle"
+        self.weights_dir = self.workdir + f"/weights/continuous/{self.tors}-{self.tant}"
+        self.pred_dir = self.workdir + f"/result/continuous/thailand/{self.resolution}"
+        self.pred_path = self.pred_dir + f"/epoch{self.epochs}_batch{self.batch_size}_seed{self.seed}.npy"
+        # model
+        self.lat, self.lon = 24, 72
+        self.lr = 0.001
+        self.lat_grid, self.lon_grid = 20, 20
+        self.grid_num = self.lat_grid*self.lon_grid
+        # init_model is allowd to be called once otherwise layer_name will be messed up
+        self.model = init_model(lat=self.lat, lon=self.lon, var_num=self.var_num, lr=self.lr)
+
+        # validation
+        self.diff_bar_view_flag = False
+        self.true_false_view_flag = True
+
+    def load_pred(self):
+        x_val, y_val = open_pickle(self.val_path)
+        if os.path.exists(self.pred_path):
+            pred_arr = np.squeeze(np.load(self.pred_path))
+        else:
+            pred_lst = []
+            for i in range(self.grid_num):
+                weights_path = f"{self.weights_dir}/epoch{self.epochs}_batch{self.batch_size}_{i}.h5"
+                model = self.model
+                model.load_weights(weights_path)
+                pred = model.predict(x_val)
+                pred_lst.append(pred)
+            pred_arr = np.squeeze(np.array(pred_lst))
+            np.save(self.pred_path, pred_arr)
+        return x_val, y_val, pred_arr # pred(400, 1000)
+
+    def diff_evaluation(self, pred, y):
+        value = pred[:, self.val_index] # pred(400, 1000)
+        label = y[self.val_index, :] # y(1000, 400)
+        diff = np.abs(value - label)
+        diff_flat = diff.reshape(-1)
+        diff_mean = np.mean(diff_flat)
+        print(diff_mean)
+        diff_bar(diff_flat)
+
+    def true_false_bar(self, pred, y, criteria=0.1):
+        true_count, false_count = 0, 0
+        for i in range(len(y)):
+            diff = np.abs(pred[:, i] - y[i, :])
+            diff_mean = np.mean(diff)
+            if diff_mean <= criteria:
+                true_count += 1
+            else:
+                false_count += 1
+        draw_val(true_count, false_count)
+
+if __name__ == '__main__':
+    main()
---
+++ continuous/thailand/different_space/main.py
@@ -0,0 +1,133 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import warnings
+warnings.filterwarnings('ignore')
+import pickle
+import numpy as np
+import tensorflow as tf
+
+from model import build_model
+from util import load, shuffle, mask
+from view import acc_map, show_map
+
+def main():
+    train_flag = False
+
+    px = Pixel()
+    if train_flag is True:
+        predictors, predictant = load(px.tors, px.tant)
+        px.training(*shuffle(predictors, predictant, px.vsample, px.seed))
+        print(f"{px.weights_dir}: SAVED")
+        print(f"{px.savefile}: SAVED")
+        px.validation()
+    else:
+        print(f"train_flag is {train_flag}: not saved")
+
+    px.show(val_index=px.val_index)
+
+class Pixel():
+    def __init__(self):
+        self.val_index = 0
+        self.epochs = 100
+        self.batch_size = 256
+        self.resolution = '1x1'
+        self.tors = 'predictors_coarse_std_Apr_msot'
+        self.tant = f"pr_{self.resolution}_std_MJJASO_thailand"
+        self.seed = 1
+        self.vsample = 1000
+        self.lat, self.lon= 24, 72
+        self.var_num = 1
+        self.lat_grid, self.lon_grid = 20, 20
+        self.grid_num = self.lat_grid * self.lon_grid
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
+        self.loss = tf.keras.losses.MeanSquaredError()
+        self.metrics = tf.keras.metrics.MeanSquaredError()
+        self.savefile = f"/docker/mnt/d/research/D2/cnn3/train_val/continuous/diff_space/{self.tors}-{self.tant}.pickle"
+        self.weights_dir = f"/docker/mnt/d/research/D2/cnn3/weights/continuous/diff_space/{self.tors}-{self.tant}"
+        self.pred_dir = f"/docker/mnt/d/research/D2/cnn3/result/continuous/thailand/{self.resolution}/diff_space"
+        self.pred_path = self.pred_dir + f"/epoch{self.epochs}_batch{self.batch_size}_seed{self.seed}.npy"
+
+    def training(self, x_train, y_train, x_val, y_val, train_dct, val_dct):
+        x_train, x_val = mask(x_train), mask(x_val)
+        x_train, x_val = x_train.transpose(0, 2, 3, 1), x_val.transpose(0, 2, 3, 1)
+        y_train, y_val = y_train.reshape(len(y_train), self.grid_num), y_val.reshape(len(y_val), self.grid_num)
+        os.makedirs(self.weights_dir, exist_ok=True)
+        for i in range(self.grid_num):
+            y_train_px = y_train[:, i]
+            model = build_model((self.lat, self.lon, self.var_num))
+            model.compile(optimizer=self.optimizer, loss=self.loss, metrics=[self.metrics])
+            his = model.fit(x_train, y_train_px, batch_size=self.batch_size, epochs=self.epochs)
+            weights_path = f"{self.weights_dir}/epoch{self.epochs}_batch{self.batch_size}_{i}.h5"
+            model.save_weights(weights_path)
+        dct = {'x_train': x_train, 'y_train': y_train,
+               'x_val': x_val, 'y_val': y_val,
+               'train_dct': train_dct, 'val_dct': val_dct}
+        os.makedirs(os.path.dirname(self.savefile), exist_ok=True)
+        with open(self.savefile, 'wb') as f:
+            pickle.dump(dct, f)
+
+    def validation(self):
+        with open(self.savefile, 'rb') as f:
+            data = pickle.load(f)
+        x_val, y_val = data['x_val'], data['y_val']
+
+        pred_lst = []
+        rmse = []
+        corr = []
+        rr = []
+        for i in range(self.grid_num):
+            y_val_px = y_val[:, i]
+            model = build_model((self.lat, self.lon, self.var_num))
+            model.compile(optimizer=self.optimizer, loss=self.loss, metrics=[self.metrics])
+            weights_path = f"{self.weights_dir}/epoch{self.epochs}_batch{self.batch_size}_{i}.h5"
+            model.load_weights(weights_path)
+
+            pred = model.predict(x_val) # (400, 1000)
+            pred_lst.append(pred)
+
+            result = model.evaluate(x_val, y_val_px)
+            rmse.append(round(result[1], 2))
+
+            pred = model.predict(x_val)
+            corr_i = np.corrcoef(pred[:,0], y_val_px)
+            corr.append(np.round(corr_i[0,1], 2))
+
+            rr_i = corr_i**2
+            rr.append(np.round(rr_i, 2))
+
+            print(f"Correlation Coefficient of pixel{i}: {np.round(corr_i[0,1], 2)}")
+
+        pred_arr = np.array(pred_lst)
+        os.makedirs(self.pred_dir, exist_ok=True)
+        np.save(self.pred_path, pred_arr)
+
+        corr = np.array(corr)
+        corr = corr.reshape(self.lat_grid, self.lon_grid)
+        acc_map(corr)
+
+    def show(self, val_index=0):
+        with open(self.savefile, 'rb') as f:
+            data = pickle.load(f)
+        x_val, y_val = data['x_val'], data['y_val']
+        y_val_px = y_val[val_index].reshape(self.lat_grid, self.lon_grid)
+        show_map(y_val_px)
+
+        pred_lst = []
+        if os.path.exists(self.pred_path) is True:
+            pred_val = np.squeeze(np.load(self.pred_path))
+            pred_arr = pred_val[:, val_index]
+        else:
+            for i in range(self.grid_num):
+                model = build_model((self.lat, self.lon, self.var_num))
+                model.compile(optimizer=self.optimizer, loss=self.loss, metrics=[self.metrics])
+                weights_path = f"{self.weights_dir}/epoch{self.epochs}_batch{self.batch_size}_{i}.h5"
+                model.load_weights(weights_path)
+                pred = model.predict(x_val)
+                result = pred[val_index]
+                pred_lst.append(result)
+            pred_arr = np.array(pred_lst)
+        pred_arr = pred_arr.reshape(self.lat_grid, self.lon_grid)
+        show_map(pred_arr)
+
+if __name__ == '__main__':
+    main()
