fb0fc8b9c61c827bf9be06e8f6cbc5691a8f428a
HalKiyo <hasegawa.k.ai@m.titech.ac.jp>
2023-06-08 17:30:33 +0900

    continuous train added

--- parallel_model/train.py
+++ parallel_model/train.py
@@ -10,11 +10,12 @@ import matplotlib.pyplot as plt
 from util import load, shuffle, mask
 from class_model import build_class_model
 from continuous_model import build_continuous_model
-from view_tmp import true_false_bar, show_class, accuracy_map
+from view import true_false_bar, show_class, accuracy_map, ACC_map, show_continuous
 
 def main():
     ################### EDIT HERE #####################
     train_flag = False
+    over_write_flag = False
     ###################################################
 
     px = Pixel()
@@ -41,12 +42,14 @@ def main():
         print(f"train_flag is {train_flag}: not saved")
 
     # class validation
-    px.validation_class()
+    px.validation_class(overwrite=over_write_flag)
     px.draw_class(val_index=px.val_index)
-    px.labelwise_accuracy_singlegrid(px_index=150)
+    px.labelwise_accuracy_singlegrid(px_index=px.px_index)
     px.labelwise_accuracy_gridmean()
 
     # continuous validation
+    px.validation_continuous(overwrite=over_write_flag)
+    px.draw_continous(val_index=px.val_index)
     plt.show()
 
 
@@ -54,6 +57,7 @@ class Pixel():
     def __init__(self):
         ########################### EDIT HERE ##################################
         ########################### common setting #############################
+        self.px_index = 150
         self.val_index = 20 #true_index=330, false_index=20
         self.resolution = '1x1' # 1x1 or 5x5_coarse
         self.var_num = 4
@@ -91,9 +95,11 @@ class Pixel():
         self.continuous_train_val_path = self.dir + f"/train_val/continuous/{self.tors}-{self.continuous_tand}.pickle"
         self.continuous_weights_dir = self.dir + f"/weights/continuous/{self.tors}-{self.continuous_tand}"
         self.continuous_result_dir = self.dir + f"/result/continuous/thailand/{self.resolution}/{self.tors}-{self.continuous_tand}"
-        self.continuous_result_path = self.continuous_result_dir + f"/epoch{self.continuous_epochs}_batch{self.batch_size}_sedd{self.seed}.npy"
+        self.continuous_result_path = self.continuous_result_dir + f"/epoch{self.continuous_epochs}_batch{self.batch_size}_seed{self.seed}.npy"
         ########################################################################
 
+######################## TRAINING START ###############################################
+#######################################################################################
     def train_class(self, x_train, y_train, x_val, y_val, train_dct, val_dct):
         x_train = mask(x_train)
         x_train = x_train.transpose(0, 2, 3, 1)
@@ -165,8 +171,9 @@ class Pixel():
 ######################## TRAINING DONE ################################################
 #######################################################################################
 
-
-    def validation_class(self):
+######################## CLASS VALIDATION  ############################################
+#######################################################################################
+    def validation_class(self, overwrite=False):
         """
         averaged gridwise accuracy map 20x20 
         (ratio of true or false in grids)
@@ -176,38 +183,7 @@ class Pixel():
         x_val, y_val = data['x_val'], data['y_val']
 
         # check if result_path exists
-        if os.path.exists(self.class_result_path) is True:
-            pred_arr = np.load(self.class_result_path)
-            y_val_lst = []
-            for i in range(self.grid_num):
-                y_val_px = y_val[:, i]
-                y_val_one_hot = tf.keras.utils.to_categorical(y_val_px, 
-                                                              self.class_num)
-                y_val_lst.append(y_val_one_hot)
-
-            pred_arr = pred_arr.reshape(self.grid_num,
-                                        self.vsample,
-                                        self.class_num)
-            y_val_arr = np.array(y_val_lst).reshape(self.grid_num,
-                                                    self.vsample,
-                                                    self.class_num)
-
-            acc_lst = []
-            for g in range(self.grid_num):
-                pred_px = pred_arr[g, :, :]
-                y_px = y_val_arr[g, :, :]
-                val_true = 0
-                val_false = 0
-                for i, j in zip(pred_px, y_px):
-                    if np.argmax(i) == np.argmax(j):
-                        val_true += 1
-                    else:
-                        val_false += 1
-                rate = (val_true)/(val_true + val_false)
-                acc_lst.append(rate)
-            acc = np.array(acc_lst).reshape(self.lat_grid, self.lon_grid)
-
-        else:
+        if os.path.exists(self.class_result_path) is False or overwrite is True:
             pred_lst = []
             acc = []
             for i in range(self.grid_num):
@@ -241,6 +217,36 @@ class Pixel():
                     os.makedirs(self.class_result_dir, exist_ok=True)
                 np.save(self.class_result_path, pred_arr)
                 print(f"{self.class_result_path} is saved")
+        else:
+            pred_arr = np.load(self.class_result_path)
+            y_val_lst = []
+            for i in range(self.grid_num):
+                y_val_px = y_val[:, i]
+                y_val_one_hot = tf.keras.utils.to_categorical(y_val_px, 
+                                                              self.class_num)
+                y_val_lst.append(y_val_one_hot)
+
+            pred_arr = pred_arr.reshape(self.grid_num,
+                                        self.vsample,
+                                        self.class_num)
+            y_val_arr = np.array(y_val_lst).reshape(self.grid_num,
+                                                    self.vsample,
+                                                    self.class_num)
+
+            acc_lst = []
+            for g in range(self.grid_num):
+                pred_px = pred_arr[g, :, :]
+                y_px = y_val_arr[g, :, :]
+                val_true = 0
+                val_false = 0
+                for i, j in zip(pred_px, y_px):
+                    if np.argmax(i) == np.argmax(j):
+                        val_true += 1
+                    else:
+                        val_false += 1
+                rate = (val_true)/(val_true + val_false)
+                acc_lst.append(rate)
+            acc = np.array(acc_lst).reshape(self.lat_grid, self.lon_grid)
 
         # view accuracy
         accuracy_map(acc)
@@ -355,7 +361,84 @@ class Pixel():
                                              class_num=self.class_num)
         print(f"class_label: {class_label}\n" \
               f"counts: {counts}")
+######################## CLASS VALIDATION  ############################################
+#######################################################################################
 
+
+######################## CONTINOUS VALIDATION  ########################################
+#######################################################################################
+    def validation_continuous(self, overwrite=False):
+        with open(self.continuous_train_val_path, 'rb') as f:
+            data = pickle.load(f)
+        x_val, y_val = data['x_val'], data['y_val']
+
+        if os.path.exists(self.continuous_result_path) is False or overwrite is True:
+            pred_lst = []
+            rmse = []
+            corr = []
+            for i in range(self.grid_num):
+                y_val_px = y_val[:, i]
+                model = build_continuous_model((self.lat, self.lon, self.var_num))
+                model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
+                              loss=self.continuous_loss,
+                              metrics=[self.continuous_metrics])
+                continuous_weights_path = f"{self.continuous_weights_dir}/epoch{self.continuous_epochs}_batch{self.batch_size}_{i}.h5"
+                model.load_weights(continuous_weights_path)
+
+                pred = model.predict(x_val) # (400, 1000)
+                pred_lst.append(pred)
+
+                result = model.evaluate(x_val, y_val_px)
+                rmse.append(round(result[1], 2))
+
+                pred = model.predict(x_val)
+                corr_i = np.corrcoef(pred[:, 0], y_val_px)
+                corr.append(np.round(corr_i[0, 1], 2))
+
+            pred_arr = np.array(pred_lst)
+            os.makedirs(self.continuous_result_dir, exist_ok=True)
+            np.save(self.continuous_result_path, pred_arr)
+            print(f"{self.continuous_result_path}: SAVED")
+
+        else:
+            corr = []
+            pred_arr = np.squeeze(np.load(self.continuous_result_path))
+            for i in range(self.grid_num):
+                y_val_px = y_val[:, i]
+                corr_i = np.corrcoef(pred_arr[i, :], y_val_px)
+                corr.append(np.round(corr_i[0, 1], 2))
+
+            corr = np.array(corr)
+            corr = corr.reshape(self.lat_grid, self.lon_grid)
+            ACC_map(corr)
+
+    def draw_continous(self, val_index=0):
+        with open(self.continuous_train_val_path, 'rb') as f:
+            data = pickle.load(f)
+        x_val, y_val = data['x_val'], data['y_val']
+        y_val_px = y_val[val_index].reshape(self.lat_grid, self.lon_grid)
+        show_continuous(y_val_px)
+
+        pred_lst = []
+        if os.path.exists(self.continuous_result_path) is True:
+            pred_val = np.squeeze(np.load(self.continuous_result_path))
+            pred_arr = pred_val[:, val_index]
+        else:
+            for i in range(self.grid_num):
+                model = build_continuous_model((self.lat, self.lon, self.var_num))
+                model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
+                              loss=self.continuous_loss,
+                              metrics=[self.continuous_metrics])
+                continuous_weights_path = f"{self.continuous_weights_dir}/epoch{self.continuous_epochs}_batch{self.batch_size}_{i}.h5"
+                model.load_weights(continuous_weights_path)
+                pred = model.predict(x_val) # (400, 1000)
+                result = pred[val_index]
+                pred_lst.append(result)
+            pred_arr = np.array(pred_lst)
+        pred_arr = pred_arr.reshape(self.lat_grid, self.lon_grid)
+        show_continuous(pred_arr)
+######################## CONTINOUS VALIDATION  ########################################
+#######################################################################################
 
 if __name__ == '__main__':
     main()
--- parallel_model/view.py
+++ parallel_model/view.py
@@ -1,43 +1,13 @@
 import numpy as np
+import cartopy.crs as ccrs
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
-import cartopy.crs as ccrs
+import colormaps as clm
+from matplotlib.colors import Normalize
 
-def true_false_bar(val_pred, val_label_onehot, class_num=5):
-    plt.rcParams["font.size"] = 18
-    fig = plt.figure()
-    ax = plt.subplot()
-
-    val_list = [i for i in range(class_num)]
-    width, linewidth, align = 0.5, 0.5, 'center'
-
-    # count
-    val_true = []
-    val_false = []
-    for i, j in zip(val_pred, val_label_onehot):
-        if np.argmax(i) == np.argmax(j):
-            val_true.append(val_list[np.argmax(j)])
-        else:
-            val_false.append(val_list[np.argmax(j)])
-
-    # true
-    val_tcount = [val_true.count(j) for j in val_list]
-    ax.bar(val_list, val_tcount,
-           color='darkslategray', width=width, linewidth=linewidth, align=align)
-
-    # false
-    val_fcount = [val_false.count(j) for j in val_list]
-    ax.bar(val_list, val_fcount,
-           color='darkgoldenrod', bottom=val_tcount, width=width, linewidth=linewidth, align=align, alpha=.8)
-
-    # count
-    val_label_class =  [np.argmax(i) for i in val_label_onehot]
-    u, counts = np.unique(val_label_class, return_counts=True)
-
-    plt.show(block=False)
-    return u, counts
-
+######################### CLASS ############################################
+############################################################################
 def show_class(image, class_num=5, lat_grid=4, lon_grid=4, txt_flag=False):
     cmap = plt.cm.get_cmap('BrBG', class_num)
     bounds = [i -0.5 for i in range(class_num+1)]
@@ -79,6 +49,40 @@ def show_class(image, class_num=5, lat_grid=4, lon_grid=4, txt_flag=False):
 
     plt.show(block=False)
 
+def true_false_bar(val_pred, val_label_onehot, class_num=5):
+    plt.rcParams["font.size"] = 18
+    fig = plt.figure()
+    ax = plt.subplot()
+
+    val_list = [i for i in range(class_num)]
+    width, linewidth, align = 0.5, 0.5, 'center'
+
+    # count
+    val_true = []
+    val_false = []
+    for i, j in zip(val_pred, val_label_onehot):
+        if np.argmax(i) == np.argmax(j):
+            val_true.append(val_list[np.argmax(j)])
+        else:
+            val_false.append(val_list[np.argmax(j)])
+
+    # true
+    val_tcount = [val_true.count(j) for j in val_list]
+    ax.bar(val_list, val_tcount,
+           color='darkslategray', width=width, linewidth=linewidth, align=align)
+
+    # false
+    val_fcount = [val_false.count(j) for j in val_list]
+    ax.bar(val_list, val_fcount,
+           color='darkgoldenrod', bottom=val_tcount, width=width, linewidth=linewidth, align=align, alpha=.8)
+
+    # count
+    val_label_class =  [np.argmax(i) for i in val_label_onehot]
+    u, counts = np.unique(val_label_class, return_counts=True)
+
+    plt.show(block=False)
+    return u, counts
+
 def accuracy_map(acc, lat_grid=4, lon_grid=4):
     projection = ccrs.PlateCarree(central_longitude=180)
     img_extent = (-90, -70, 5, 25) # location = (N5-25, E90-110)
@@ -106,55 +110,42 @@ def accuracy_map(acc, lat_grid=4, lon_grid=4):
     plt.rcParams["font.size"] = 18
     plt.show(block=False)
 
-def pred_accuracy(true_count, false_count):
+######################### CONTINUOUS #######################################
+############################################################################
+def show_continuous(image, vmin=-1, vmax=1):
+    cmap = plt.cm.get_cmap('BrBG')
+    projection = ccrs.PlateCarree(central_longitude=180)
+    img_extent = (-90, -70, 5, 25) # location = (N5-25, E90-110)
+
     fig = plt.figure()
-    ax = plt.subplot()
-    print(true_count, false_count)
-    ax.barh(1, true_count, height=0.5, color='darkslategray', align='center', label='True')
-    ax.barh(1, false_count, left=true_count, height=0.5, color='orange', align='center', label='False')
-    ax.set_ylim(0,2)
-    ax.set_yticks([1.0], ['val'])
-    plt.legend()
-    plt.rcParams["font.size"] = 18
+    ax = plt.subplot(projection=projection)
+    ax.coastlines()
+    mat = ax.matshow(image,
+                     origin='upper',
+                     extent=img_extent,
+                     transform=projection,
+                     norm=Normalize(vmin=vmin, vmax=vmax),
+                     cmap=cmap)
+    fig.colorbar(mat, ax=ax)
     plt.show(block=False)
 
-def bimodal_dist(true_count_of_each_individual_validation_sample):
-    fig, ax = plt.subplots()
-    ax.hist(true_count_of_each_individual_validation_sample, color='g', alpha=0.5)
-    plt.show(block=False)
+def ACC_map(ACC, lat_grid=20, lon_grid=20, vmin=0.75, vmax=1.00, discrete=5):
+    projection = ccrs.PlateCarree(central_longitude=180)
+    img_extent = (-90, -70, 5, 25) # location = (N5-25, E90-110)
 
-def view_probability(pred, flag=False):
+    mpl.colormaps.unregister('ACC_map')
+    mpl.colormaps.register(clm.temps, name='ACC_map')
+    cm = plt.cm.get_cmap('ACC_map', discrete)
+
     fig = plt.figure()
-    ax = plt.subplot()
-    width, linewidth, align = 0.5, 0.5, 'center'
-    if flag is False:
-        color='darkgoldenrod'
-    else:
-        color='darkslategray'
-
-    ticks = np.arange(len(pred))
-    ax.bar(ticks,
-           pred,
-           color=color,
-           width=width,
-           linewidth=linewidth,
-           align=align)
-    ax.set_ylim(0,1)
-    plt.rcParams["font.size"] = 18
-    plt.show(block=False)
-
-def box_crossentropy(true, false, class_num=5):
-    plt.rcParams["font.size"] = 18
-    label = np.arange(1)
-    xs = {key:val for key, val in zip(true.keys(), label)}
-    shift = 0.1
-
-    fig, ax = plt.subplots()
-    for key in true.keys():
-        ax.boxplot(true[key], positions=[xs[key] - shift], boxprops=dict(color='darkslategray'),
-                   showfliers=False)
-        ax.boxplot(false[key], positions=[xs[key] + shift], boxprops=dict(color='darkgoldenrod'),
-                   showfliers=False)
-    ax.set_xticks(range(1))
-    ax.set_xticklabels(true.keys())
+    ax = plt.subplot(projection=projection)
+    ax.coastlines()
+    mat = ax.matshow(ACC,
+                     origin='upper',
+                     extent=img_extent,
+                     transform=projection,
+                     vmin=vmin,
+                     vmax=vmax,
+                     cmap=cm)
+    fig.colorbar(mat, ax=ax)
     plt.show(block=False)
